fc038841a483d6cf8b4c577b66005effb69a6694
gauravshelke-dev <gauravshelke226@gmail.com>
2025-02-20 16:34:53 +0530

    feature of subtraction added into calculator

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -3,6 +3,9 @@ class Calculator{
     public int add(int a,int b){
         return a + b;
     }
+    public int subtract(int a,int b){
+        return a - b;
+    }
 }
 
 
@@ -10,5 +13,6 @@ public  class CalculatorTest{
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("Result" + calc.add(10,20));
+        System.out.println("Result" + calc.subtract(20,10));
     }
 }
