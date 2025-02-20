1c15f3c280290b7da997781468c24536c1c7624e
Gaurav Shelke <gauravshelke226@gmail.com>
2025-02-20 17:28:55 +0530

    added mult feature

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -4,6 +4,10 @@ class Calculator{
     public int subtract(int a,int b){
         return a - b;
     }
+    
+    public int mult(int a,int b){
+        return a * b;
+    }
 }
 
 
@@ -12,5 +16,6 @@ public  class CalculatorTest{
         Calculator calc = new Calculator();
         // System.out.println("Result" + calc.add(10,20));
         System.out.println("Result" + calc.subtract(20,10));
+        System.out.println("Result" + calc.mult(20,10));
     }
 }
